523a62703a88ee131290cce46f9291a99c562117
gaabrielfranco <gabriel.vita@ufv.br>
2018-12-01 15:21:14 -0200

    Reading the input

--- set-cover/main.cpp
+++ set-cover/main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
-    std::cout << "Hello world!\n";
+    size_t n, m, element;
+    char c;
+
+    scanf("%zu %zu", &n, &m);
+    std::vector<std::vector<size_t>> G;
+    size_t* costs = (size_t*)malloc(m * sizeof(size_t));
+    std::vector<size_t> v;
+
+    for (size_t i = 0; i < m; i++)
+    {
+        while (scanf("%zu%c", &element, &c))
+        {
+            v.push_back(element);
+
+            if (c == '\n')
+            {
+                G.push_back(v);
+                v.clear();
+                break;
+            }
+        }
+    }
+
+    for (size_t i = 0; i < m; i++)
+    {
+        scanf("%zu", &costs[i]);
+    }
+
+    for (size_t i = 0; i < G.size(); i++)
+    {
+        for (size_t j = 0; j < G[i].size(); j++)
+        {
+            std::cout << G[i][j] << " ";
+        }
+        puts("");
+    }
+
+    for (size_t i = 0; i < m; i++)
+    {
+        printf("%zu ", costs[i]);
+    }
+    puts("");
+
     return 0;
 }
